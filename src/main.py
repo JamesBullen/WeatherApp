ad49d6e0b1bb129c60f7d1800701a0c1b5988d08
JamesBullen <james.roberts.bullen@gmail.com>
2024-08-15 10:51:48 +0100

    Allows to be called as an API now

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
 from search import fetchCoordsLoop
 from weather import fetchWeather
-from report import printReport
+from report import printReport, getForecast
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 def main():
-    coords, location = fetchCoordsLoop()
+    coords, address = fetchCoordsLoop()
     weather = fetchWeather(coords)
 
-    printReport(weather, location)
+    printReport(weather, address)
 
-main()
+@app.route('/<location>')
+def weather(location):
+    coords, address = fetchCoordsLoop(location)
+    weather = fetchWeather(coords)
+    weather[1] = getForecast(weather[1])
+
+    return jsonify(weather, address), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
