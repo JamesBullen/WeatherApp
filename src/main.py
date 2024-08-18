857481499ed67bedb88c8b78a0197871b55a0435
JamesBullen <james.roberts.bullen@gmail.com>
2024-08-18 16:43:07 +0100

    Added comments

--- src/main.py
+++ src/main.py
@@ -5,19 +5,24 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Only used to check the weather from an interal command
 def main():
     coords, address = fetchCoordsLoop()
     weather = fetchWeather(coords)
 
     printReport(weather, address)
 
+# API function
 @app.route('/<location>')
 def weather(location):
     coords, address = fetchCoordsLoop(location)
     weather = fetchWeather(coords)
     weather[1] = getForecast(weather[1])
 
-    return jsonify(weather, address), 200
+    response = jsonify(weather, address)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
