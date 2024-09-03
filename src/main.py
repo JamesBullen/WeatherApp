e990df78248a255fad42f7967f92993975067ecb
JamesBullen <james.roberts.bullen@gmail.com>
2024-09-03 16:38:51 +0100

    Added proper CORS policy compliance

--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
 from search import fetchCoords
 from weather import fetchWeather
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # API function
 @app.route('/<location>/<distance>', methods=['GET'])
+@cross_origin()
 def weather(location, distance):
     addresses, coords = fetchCoords(location, int(distance))
     weather = fetchWeather(coords)
 
     response = jsonify(addresses, weather)
-    response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc')
+    app.run(host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
