046702a7715fb0242f4f575f36db3572a1dc33f5
JamesBullen <james.roberts.bullen@gmail.com>
2024-09-11 18:53:22 +0100

    added no cors option

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "corsheaders",
     "app",
 ]
 
@@ -52,6 +53,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5000',
+]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:5000',
 ]
 
 ROOT_URLCONF = 'WeatherApp.urls'
