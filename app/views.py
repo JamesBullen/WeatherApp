eebc2dffc1f9fd3fbaa4870d6699df6be02142da
JamesBullen <james.roberts.bullen@gmail.com>
2025-01-28 19:57:20 +0000

    Added check

--- app/views.py
+++ app/views.py
@@ -8,8 +8,10 @@ from weather import fetchWeather
 def temp(request, location, distance):
     print(location)
     addresses, coords = fetchCoords(location, int(distance))
+    
     print(addresses, coords)
-    weather = fetchWeather(coords)
+    if coords != False:
+        weather = fetchWeather(coords)
 
     response = (addresses, weather)
 
